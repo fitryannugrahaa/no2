a72d30aa71850ce7291da224f3629da69d58e816
fitryannugrahaa <32404079+fitryannugrahaa@users.noreply.github.com>
2017-11-08 10:32:28 +0800

    no 2

--- Untitled3.cpp
+++ Untitled3.cpp
@@ -4,4 +4,31 @@
 using namespace std;
 
 int main () {
+	char bidang;
+double p,l,a,t,L;
+
+cout << "Pilih Bidang : ";
+cin >> bidang;
+
+	if (bidang == 'P' || bidang == 'p') {	
+		cout << "   Mengitung luas persegi panjang \n";	
+		cout << "   Masukkan panjangnya : ";
+		cin >> p;
+		cout << "   Masukkan lebarnya   : ";
+		cin >> l;
+		L = p*l;
+		cout << "   Luas persegi panjang : "<< L<<endl;
+	}
+	else if (bidang == 'S' || bidang == 's') {
+		cout << "   Mengitung luas segitiga \n";	
+		cout << "   Masukkan alasnya   : ";
+		cin >> a;
+		cout << "   Masukkan tingginya : ";
+		cin >> t;
+		L = 0.5*a*t;
+		cout << "   Luas persegi panjang : "<< L<<endl;
+	}	
+	else{
+		cout<<"Maaf, Anda Salah Memilih"<<endl;
+	}
 }
